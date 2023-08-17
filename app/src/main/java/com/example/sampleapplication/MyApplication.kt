5fa9f1218f6b1b3d3b76da89edfc0064a82eda31
Seetha Chakkapalli <srilakshmi.chakkapalli@gmail.com>
2023-08-17 16:56:15 +0530

    injected app component graph in application class

--- app/src/main/java/com/example/sampleapplication/MyApplication.kt
+++ app/src/main/java/com/example/sampleapplication/MyApplication.kt
@@ -1,10 +1,16 @@
 package com.example.sampleapplication
 
+import com.example.sampleapplication.di.AppComponent
+import com.example.sampleapplication.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
 class MyApplication: DaggerApplication() {
+
+    private lateinit var appComponent: AppComponent
+
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-        TODO("Not yet implemented")
+        appComponent = DaggerAppComponent.builder().application(this).build()
+        return appComponent
     }
 }
