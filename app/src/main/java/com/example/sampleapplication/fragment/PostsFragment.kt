7921635a3e60148606af5e91649199464acea59f
Seetha Chakkapalli <srilakshmi.chakkapalli@gmail.com>
2023-08-17 15:40:33 +0530

    inflated layouts of fragments using generated binding classes

--- app/src/main/java/com/example/sampleapplication/fragment/PostsFragment.kt
+++ app/src/main/java/com/example/sampleapplication/fragment/PostsFragment.kt
@@ -6,19 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.sampleapplication.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
+import com.example.sampleapplication.databinding.FragmentPostsBinding
 
 class PostsFragment : Fragment() {
+
+    private lateinit var binding: FragmentPostsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_posts, container, false)
+    ): View {
+        binding = FragmentPostsBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 }
