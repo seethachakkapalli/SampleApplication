37c77a21d1a9ebd60ca8a0d42b19ec6a20645ef8
Seetha Chakkapalli <srilakshmi.chakkapalli@gmail.com>
2023-08-18 01:16:28 +0530

    fetched data from remote repository into viewmodel and exposed it as livedata

--- app/src/main/java/com/example/sampleapplication/viewmodel/PostViewModel.kt
+++ app/src/main/java/com/example/sampleapplication/viewmodel/PostViewModel.kt
@@ -3,12 +3,20 @@ package com.example.sampleapplication.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.sampleapplication.model.Post
+import com.example.sampleapplication.repository.Repository
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import javax.inject.Inject
 
 class PostViewModel: ViewModel() {
 
-    var post: MutableLiveData<Post>? = null
+    var posts: MutableLiveData<List<Post>>? = null
+
+    @Inject lateinit var repository: Repository
 
     fun getPosts() {
-
+        repository.getPosts().subscribe {
+            list ->
+            posts?.value = list
+        }
     }
 }
