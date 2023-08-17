f87d0232385bc6c8301828040777d522f508f698
Seetha Chakkapalli <srilakshmi.chakkapalli@gmail.com>
2023-08-17 17:19:57 +0530

    added interceptor for adding headers and also checking response before coming to the user

--- app/src/main/java/com/example/sampleapplication/service/ApiInterceptor.kt
+++ app/src/main/java/com/example/sampleapplication/service/ApiInterceptor.kt
@@ -1,10 +1,27 @@
 package com.example.sampleapplication.service
 
+import android.util.Log
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
+import java.lang.Exception
 
 class ApiInterceptor: Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        TODO("Not yet implemented")
+        val original: Request = chain.request()
+        //header customization
+        //sending token
+        //create a request builder to transform the request and add headers
+        val response: Response = chain.proceed(request = original)
+
+        try {
+            if (response.code == 400) {
+                Log.e("Response unsuccessfull", response.message);
+            }
+        } catch (e: Exception) {
+            chain.proceed(original)
+        }
+
+        return response
     }
 }
