86eafe68917f797f8954223dd6beb79d04056396
Seetha Chakkapalli <srilakshmi.chakkapalli@gmail.com>
2023-08-17 17:31:39 +0530

    added library for loggin interceptor and added logging interceptor to okhttp client

--- app/src/main/java/com/example/sampleapplication/di/AppModule.kt
+++ app/src/main/java/com/example/sampleapplication/di/AppModule.kt
@@ -3,6 +3,7 @@ package com.example.sampleapplication.di
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,7 +12,10 @@ class AppModule {
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
         return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
             .build()
     }
 
