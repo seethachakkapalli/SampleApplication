a8816a7f86ba29d50407dd9188a2b3937d3c302a
Seetha Chakkapalli <srilakshmi.chakkapalli@gmail.com>
2023-08-18 01:14:19 +0530

    made viewmodel factory class ready to inject viewmodels into activities and fragments using map

--- app/src/main/java/com/example/sampleapplication/ViewModelFactory.kt
+++ app/src/main/java/com/example/sampleapplication/ViewModelFactory.kt
@@ -6,9 +6,28 @@ import javax.inject.Inject
 import javax.inject.Provider
 
 class ViewModelFactory @Inject constructor(
-    private val viewModelMap: Map<Class<out ViewModel>, Provider<ViewModel>>
-): ViewModelProvider.Factory {
+    private val creators: Map<Class<out ViewModel>, Provider<ViewModel>>
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        var creator: Provider<out ViewModel>? = creators[modelClass]
+        if (creator == null) {
+            for ((key, value) in creators) {
+                if (modelClass.isAssignableFrom(key)) {
+                    creator = value
+                    break
+                }
+            }
+        }
+        if (creator == null) {
+            throw IllegalArgumentException("unknown model class " + modelClass)
+        }
+        try {
+            return creator.get() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
 
+    }
 }
 
 
