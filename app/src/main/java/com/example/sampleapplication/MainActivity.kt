397dd7f2c11b2c8653155afa787add163d3c2d11
Seetha Chakkapalli <srilakshmi.chakkapalli@gmail.com>
2023-08-17 15:21:14 +0530

    changed to data binding layout and inflating layout from activity using databinding util

--- app/src/main/java/com/example/sampleapplication/MainActivity.kt
+++ app/src/main/java/com/example/sampleapplication/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.sampleapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.sampleapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
