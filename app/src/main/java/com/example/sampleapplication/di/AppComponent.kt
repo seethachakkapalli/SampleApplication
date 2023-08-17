3a01771bfeb26a3874c3a1dd823bc211d7ebf138
Seetha Chakkapalli <srilakshmi.chakkapalli@gmail.com>
2023-08-17 16:41:00 +0530

    extending app component class with AndroidInjector to provide dependencies to application class

--- app/src/main/java/com/example/sampleapplication/di/AppComponent.kt
+++ app/src/main/java/com/example/sampleapplication/di/AppComponent.kt
@@ -1,7 +1,10 @@
 package com.example.sampleapplication.di
 
+import com.example.sampleapplication.MyApplication
 import dagger.Component
+import dagger.android.AndroidInjector
 
+//Extending AndroidInjector means it is responsible for injecting dependencies into application class
 @Component
-interface AppComponent {
+interface AppComponent: AndroidInjector<MyApplication> {
 }
