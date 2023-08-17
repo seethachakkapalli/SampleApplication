87cf16638727d363ca17083a844942bbb973e9a0
Seetha Chakkapalli <srilakshmi.chakkapalli@gmail.com>
2023-08-17 18:29:59 +0530

    added endpoint for get posts

--- app/src/main/java/com/example/sampleapplication/service/ApiService.kt
+++ app/src/main/java/com/example/sampleapplication/service/ApiService.kt
@@ -1,5 +1,12 @@
 package com.example.sampleapplication.service
 
+import com.example.sampleapplication.model.Post
+import io.reactivex.rxjava3.core.Flowable
+import retrofit2.http.GET
+
 //this interface is responsible for defining API endpoints
 interface ApiService {
+
+    @GET("/posts")
+    fun getPosts() : Flowable<Post>
 }
