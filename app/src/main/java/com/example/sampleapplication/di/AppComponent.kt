936d22431ad5c41b604a724579f7daeb00b89756
Seetha Chakkapalli <srilakshmi.chakkapalli@gmail.com>
2023-08-17 16:55:48 +0530

    added modules - AndroidInjectionModule in appcomponent to support injection in application class

--- app/src/main/java/com/example/sampleapplication/di/AppComponent.kt
+++ app/src/main/java/com/example/sampleapplication/di/AppComponent.kt
@@ -4,10 +4,15 @@ import android.app.Application
 import com.example.sampleapplication.MyApplication
 import dagger.BindsInstance
 import dagger.Component
+import dagger.android.AndroidInjectionModule
 import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
 
 //Extending AndroidInjector means it is responsible for injecting dependencies into application class
-@Component
+//modules = [(AndroidInjectionModule::class)] should be added to inject in the application class
+
+// AndroidInjectionModule ===> Application class related module
+@Component(modules = [(AndroidInjectionModule::class), AndroidSupportInjectionModule::class])
 interface AppComponent: AndroidInjector<MyApplication> {
 
     @Component.Builder
