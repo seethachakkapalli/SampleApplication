0a834ea4b3e2c87892db219c387ad4f255800f6e
Seetha Chakkapalli <srilakshmi.chakkapalli@gmail.com>
2023-08-17 17:25:36 +0530

    create app module to provide retrofit instances to the overall application

---
+++ app/src/main/java/com/example/sampleapplication/di/AppModule.kt
@@ -0,0 +1,26 @@
+package com.example.sampleapplication.di
+
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+class AppModule {
+
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .build()
+    }
+
+    @Provides
+    fun provideRetrofitInstance(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+}
