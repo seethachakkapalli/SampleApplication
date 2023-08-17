438da9de726bbc02bda5d53f4f799df77b2aa266
Seetha Chakkapalli <srilakshmi.chakkapalli@gmail.com>
2023-08-18 01:05:25 +0530

    added new key for formatting response by adding fetched time to each post

--- app/src/main/java/com/example/sampleapplication/model/Post.kt
+++ app/src/main/java/com/example/sampleapplication/model/Post.kt
@@ -1,3 +1,4 @@
 package com.example.sampleapplication.model
 
-data class Post(val userId: Int, val id: Int, val title: String, val body: String)
+
+data class Post(val userId: Int, val id: Int, val title: String, val body: String, var fetchedOn: String)
