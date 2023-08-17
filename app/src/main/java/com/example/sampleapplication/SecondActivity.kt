2b054d8ba1f0c2cf9dd9f32c61fc2f6fcd42e016
Seetha Chakkapalli <srilakshmi.chakkapalli@gmail.com>
2023-08-17 19:01:52 +0530

    created second activity and created second_nav_graph

--- app/src/main/java/com/example/sampleapplication/SecondActivity.kt
+++ app/src/main/java/com/example/sampleapplication/SecondActivity.kt
@@ -2,10 +2,16 @@ package com.example.sampleapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sampleapplication.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
